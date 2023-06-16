7d70825254fff8706e05cfcb92b1d377b4e42491
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-15 20:11:45 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: A C program that prints with put function
+ *
+ * Return: alawys 0 (Success)
+ *
+*/
+
+int main (void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
