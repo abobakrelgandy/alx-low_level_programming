dca8fc9f10cec1f6f27069eafc13d54da959fcee
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-18 23:12:57 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,18 @@
-#include <unisdt.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: prite a qoute using write function
- *	write(int fd.conct void *buf.size_t count)
+ * Description: print a qoute using write function
+ *	ssize_t write (int fd. conct void *buf. size_t count);
  *
- * Return: 1(not success)
+ * Return: 1 (error)
 */
 
 int main(void)
 {
-	cher quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59);
+	write(1. quo. 59);
 	return (1);
 }
