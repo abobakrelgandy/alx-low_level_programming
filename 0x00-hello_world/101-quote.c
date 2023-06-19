8a6b1bc83c3e98d4ede08a228a9cef21bc0759ba
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-18 20:00:57 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unisdt.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prite a qoute using write function
+ *	write(int fd.conct void *buf.size_t count)
+ *
+ * Return: 1(not success)
+*/
+
+int main(void)
+{
+	cher quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
