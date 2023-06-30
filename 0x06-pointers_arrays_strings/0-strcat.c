4b1931c467ecf0ff3046f33711b558f73b2bbd1c
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-29 17:52:52 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #inculde "main.h"
 
 /**
- * _strcat - functions that concatenates
+ * _strcat - function that concatenates
  *		two strings.
  *
- * @dest: pointer to destanation input
- * @src: pointer to souarce input
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
 */
@@ -15,11 +15,11 @@ char *_strcat(char *dest, char *src)
 	int c, c2;
 
 	c = 0;
-	/*find the size of the dest arrey*/
+	/*find the size of dest arrey*/
 	while (dest[c])
 		c++;
 
-	/* iterate through each char src arrey value without null byte*/
+	/* iterate through each src arrey value without null byte*/
 	for (c2 = 0; src[c2]; c2++)
 		/*append src[c2] to dest[c] while overwriting the null byte in dest*/
 		dest[c++] = src[c2];
