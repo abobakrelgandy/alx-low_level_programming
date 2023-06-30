79280e56b3dee99a66c6b8600e69b9e9300a8b6e
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-30 14:01:30 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -20,8 +20,8 @@ char *_strcat(char *dest, char *src)
 		c++;
 
 	/* iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2]; c2++)
-		/*append src[c2] to dest[c] while overwriting the null byte in dest*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
 		dest[c++] = src[c2];
 
 	return (dest);
