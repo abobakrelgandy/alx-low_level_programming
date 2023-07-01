6f6634caf132a958bd8537a0789fe8b04f825e78
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-06-30 17:28:34 -0700

    a

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,7 +17,7 @@ char *leet(char *c)
 	{
 		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			/*32 is the difference between lower case letter and apper case letters*/
+			/*32 is the difference between lower case letters and apper case letters*/
 			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
