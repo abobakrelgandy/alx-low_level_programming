86aa2895e2be1e4831364b83ac6418cf0fd88dfe
abobakr elgandy <abobakrelgandy338@gmail.com>
2023-08-11 13:45:34 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,9 +11,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf{READ_BUF_SIZE * 8};
+	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename || letters)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd  == -1)
